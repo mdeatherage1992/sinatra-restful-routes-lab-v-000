45020194a9faacc8183b5f2c6e3d785fa42e267d
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-23 19:54:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    @recipes = Recipe.all
-    erb :index
-  end
+  # get '/' do
+  #   @recipes = Recipe.all
+  #   erb :index
+  # end
 
   get '/recipes' do
   @recipes = Recipe.all
@@ -28,7 +28,7 @@ end
   erb :show
 end
 
-get '/recipes/:id/edit' do  
+get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
