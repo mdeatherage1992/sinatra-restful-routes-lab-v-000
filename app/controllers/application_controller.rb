3d246d83bce039339ea6527141af2d6a3ad690c0
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-23 19:58:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 require './config/environment'
+
 class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  set :views, Proc.new { File.join(root, "../views/") }
 
   # get '/' do
   #   @recipes = Recipe.all
